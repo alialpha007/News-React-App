0e8b84d8679b43ee97c1db752767f32436090691
alialpha007 <94750994+alialpha007@users.noreply.github.com>
2022-07-28 12:30:02 +0500

    initial commit of this app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  return <div className="">This is a starter code for react.</div>;
+  return <div className="">This is a news web app in react.</div>;
 }
 
 export default App;
