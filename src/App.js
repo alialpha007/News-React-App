b2a09c3893181d3eb134f67d8bde11f27a80cdcc
alialpha007 <94750994+alialpha007@users.noreply.github.com>
2022-07-28 11:41:30 +0500

    final commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return <div className="">This is a starter code for react.</div>;
+}
+
+export default App;
